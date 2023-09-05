ab32a7ba07481c76de4656f7c78d9eab882a3b62
Stent <stentonian@protonmail.com>
2023-09-05 13:53:40 +0100

    Example usage of ndm_smt in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use std::str::FromStr;
+
+use dapol::{NdmSmt, User, D256, UserId};
+
 fn main() {
-    // TODO: implement running DAPOL from command line arguments?
-    // or, remove main.rs altogether?
+    let user1 = User{
+        liability: 10u64,
+        id: UserId::from_str("user1 ID").unwrap(),
+    };
+
+    let master_secret: D256 = D256::from(3u64);
+    let salt_b: D256 = D256::from(5u64);
+    let salt_s: D256 = D256::from(7u64);
+    let height: u8 = 5u8;
+    let users: Vec<User> = vec![user1];
+    let ndsmt = NdmSmt::new(master_secret, salt_b, salt_s, height, users).unwrap();
+
+    let proof = ndsmt.generate_inclusion_proof(&UserId::from_str("user1 ID").unwrap()).unwrap();
+    println!("{:?}", proof);
 }
