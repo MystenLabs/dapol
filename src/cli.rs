a51e0b43055afe1f041eb2170f1c5c8dadec03a5
Stent <stentonian@protonmail.com>
2023-10-11 23:06:26 +0100

    Some docs

--- src/cli.rs
+++ src/cli.rs
@@ -8,9 +8,11 @@ pub struct Args {
     #[command(flatten)]
     pub verbose: Verbosity<WarnLevel>,
 
+    /// Height to use for the binary tree.
     #[arg(long)]
     pub height: Option<u8>,
 
+    /// TOML file containing secrets (see secrets_example.toml).
     #[clap(short, long, value_parser)]
     pub secrets: Option<Input>,
 }
