477f9328cb31eea07cf02b0174d4c7963554c282
Stent <stentonian@protonmail.com>
2023-10-13 16:11:09 +0100

    Smol docs change

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,31 @@
+//! Command Line Interface.
+//!
+//! Output of `--help`:
+//! ```
+//! DAPOL+ Proof of Liabilities protocol in Rust
+//!
+//!     Usage: dapol [OPTIONS] <--entity-file <ENTITY_FILE>|--random-entities <RANDOM_ENTITIES>>
+//!
+//!     Options:
+//!         -e, --entity-file <ENTITY_FILE>
+//!             Path to file containing entity ID & liability entries (supported file types: csv)
+//!         -r, --random-entities <RANDOM_ENTITIES>
+//!             Randomly generate a number of entities
+//!         -v, --verbose...
+//!             More output per occurrence
+//!         -q, --quiet...
+//!             Less output per occurrence
+//!         --height <HEIGHT>
+//!             Height to use for the binary tree
+//!         -s, --secrets <SECRETS>
+//!             TOML file containing secrets (see secrets_example.toml)
+//!         -h, --help
+//!             Print help
+//!         -V, --version
+//!             Print version
+//! ```
+// TODO replace above help text with better description
+
 use clap::{command, Args, Parser};
 use clap_verbosity_flag::{Verbosity, WarnLevel};
 use clio::Input;
@@ -28,8 +56,6 @@ pub struct Cli {
 #[derive(Args, Debug)]
 #[group(required = true, multiple = false)]
 pub struct EntitySource {
-    // TODO say where one can find supported file formats
-    // TODO also say it supports stdin
     /// Path to file containing entity ID & liability entries (supported file
     /// types: csv).
     #[arg(short, long)]
