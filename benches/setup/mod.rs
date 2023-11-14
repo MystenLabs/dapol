abee918e26890afb68c507a507ef9b4577b44b0a
Matthew Pratt <matthew.pratt13@gmail.com>
2023-11-14 15:51:05 +0200

    add BenchTestContent type

--- benches/setup/mod.rs
+++ benches/setup/mod.rs
@@ -1,7 +1,36 @@
 use core::fmt::Debug;
 use dapol::binary_tree::{BinaryTree, Coordinate, InputLeafNode, Mergeable, TreeBuilder};
-use dapol::Height;
+use dapol::{Hasher, Height};
+use primitive_types::H256;
 use serde::Serialize;
+
+#[derive(Clone, Debug, PartialEq, Serialize)]
+pub(crate) struct BenchTestContent {
+    pub value: u32,
+    pub hash: H256,
+}
+
+impl Mergeable for BenchTestContent {
+    fn merge(left_sibling: &Self, right_sibling: &Self) -> Self {
+        // C(parent) = C(L) + C(R)
+        let parent_value = left_sibling.value + right_sibling.value;
+
+        // H(parent) = Hash(C(L) | C(R) | H(L) | H(R))
+        let parent_hash = {
+            let mut hasher = Hasher::new();
+            hasher.update(&left_sibling.value.to_le_bytes());
+            hasher.update(&right_sibling.value.to_le_bytes());
+            hasher.update(left_sibling.hash.as_bytes());
+            hasher.update(right_sibling.hash.as_bytes());
+            hasher.finalize()
+        };
+
+        BenchTestContent {
+            value: parent_value,
+            hash: parent_hash,
+        }
+    }
+}
 
 pub(crate) fn build_tree<C, F>(
     height: Height,
