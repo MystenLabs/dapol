f2315f03a0048f61039b37989592979cc1aaff4e
Matthew Pratt <matthew.pratt13@gmail.com>
2023-11-15 17:43:39 +0200

    add first few lines of bench_build_tree()

--- benches/build_tree.rs
+++ benches/build_tree.rs
@@ -1,4 +1,15 @@
 mod setup;
 
+use criterion::{Criterion, SamplingMode};
+use setup::TREE_HEIGHTS;
+use std::time::Duration;
 
+fn bench_build_tree(c: &mut Criterion) {
+    let mut group = c.benchmark_group("build");
 
+    group.sample_size(10);
+    group.sampling_mode(SamplingMode::Flat);
+    group.measurement_time(Duration::from_secs(20));
+
+    let tree_height = TREE_HEIGHTS[0];
+}
