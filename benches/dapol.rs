850c33bb047da02c57e5507ce2947ce8a0a97a64
Matthew Pratt <matthew.pratt13@gmail.com>
2023-11-24 19:50:00 +0200

    add proof verification to bench algorithm

--- benches/dapol.rs
+++ benches/dapol.rs
@@ -19,7 +19,7 @@ fn bench_dapol(c: &mut Criterion) {
     let act = stats::active::mib().unwrap();
     let res = stats::resident::mib().unwrap();
 
-    let mut group = c.benchmark_group("build");
+    let mut group = c.benchmark_group("dapol");
     group.sample_size(10);
     group.sampling_mode(SamplingMode::Flat);
 
@@ -148,6 +148,40 @@ fn bench_dapol(c: &mut Criterion) {
                     &entity_ids[0],
                     PathBuf::from("./target"),
                 );
+
+                let alloc = stats::allocated::mib().unwrap();
+                let act = stats::active::mib().unwrap();
+                let res = stats::resident::mib().unwrap();
+
+                // proof verification compute time
+                group.bench_function(
+                    BenchmarkId::new(
+                        "verify_proof",
+                        format!("{:?}/{:?}/NUM_USERS: {:?}", &tup.0, &tup.1, &tup.2),
+                    ),
+                    |bench| {
+                        bench.iter(|| {
+                            e.advance().unwrap();
+
+                            InclusionProof::verify(
+                                proof.as_ref().expect("Proof not found"),
+                                ndm_smt.as_ref().expect("Tree not found").root_hash(),
+                            )
+                            .expect("Unable to verify proof")
+                        });
+                    },
+                );
+
+                // proof verification memory usage
+                let alloc = alloc.read().unwrap();
+                let act = act.read().unwrap();
+                let res = res.read().unwrap();
+                println!(
+                    "Proof verification memory usage: {} allocated / {} active / {} resident",
+                    setup::bytes_as_string(alloc),
+                    setup::bytes_as_string(act),
+                    setup::bytes_as_string(res)
+                );
             }
         }
     }
@@ -155,140 +189,38 @@ fn bench_dapol(c: &mut Criterion) {
     group.finish()
 }
 
-// fn bench_generate_proof(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("generate");
-//     group.sample_size(10);
+// ================================================================================================
 
-//     let num_entities = NUM_USERS[2]; // 30_000: max. value for tree height 16
-//     let thread_counts: [u8; 7] = [4, 8, 16, 32, 64, 128, 255];
+fn bench_test_jemalloc_readings() {
+    let e = epoch::mib().unwrap();
+    let alloc = stats::allocated::mib().unwrap();
+    let act = stats::active::mib().unwrap();
+    let res = stats::resident::mib().unwrap();
 
-//     for h in TREE_HEIGHTS {
-//         let ndm_smt =
-//             setup::build_ndm_smt(Height::from(h), MaxThreadCount::default(), num_entities);
-//         let entity_id = EntityId::from_str("foo").unwrap();
+    // 1 MB
+    let buf: Vec<u8> = Vec::with_capacity(1024u32.pow(2) as usize);
 
-//         group.bench_function(BenchmarkId::new("tree_height", h), |bench| {
-//             bench.iter(|| {
-//                 setup::generate_proof(&ndm_smt, &entity_id);
-//             })
-//         });
-//     }
+    e.advance().unwrap();
 
-//     for t in thread_counts {
-//         let ndm_smt = setup::build_ndm_smt(Height::from(16), MaxThreadCount::from(t), num_entities);
-//         let entity_id = EntityId::from_str("foo").unwrap();
+    println!(
+        "buf capacity: {:<6}",
+        setup::bytes_as_string(buf.capacity())
+    );
 
-//         group.bench_function(BenchmarkId::new("max_thread_count", t), |bench| {
-//             bench.iter(|| {
-//                 setup::generate_proof(&ndm_smt, &entity_id);
-//             })
-//         });
-//     }
+    let alloc = alloc.read().unwrap();
+    let act = act.read().unwrap();
+    let res = res.read().unwrap();
 
-//     for u in NUM_USERS {
-//         let ndm_smt = setup::build_ndm_smt(Height::from(16), MaxThreadCount::default(), u);
-//         let entity_id = EntityId::from_str("foo").unwrap();
-
-//         group.bench_function(BenchmarkId::new("num_users", u), |bench| {
-//             bench.iter(|| setup::generate_proof(&ndm_smt, &entity_id));
-//         });
-//     }
-
-//     group.finish();
-// }
-
-// fn bench_verify_proof(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("verify");
-//     group.sample_size(10);
-
-//     let num_entities = NUM_USERS[2]; // 30_000: max. value for tree height 16
-//     let thread_counts: [u8; 7] = [4, 8, 16, 32, 64, 128, 255];
-
-//     for h in TREE_HEIGHTS {
-//         let ndm_smt =
-//             setup::build_ndm_smt(Height::from(h), MaxThreadCount::default(), num_entities);
-//         let entity_id = EntityId::from_str("foo").unwrap();
-//         let proof = setup::generate_proof(&ndm_smt, &entity_id);
-
-//         group.bench_function(BenchmarkId::new("tree_height", h), |bench| {
-//             bench.iter(|| {
-//                 proof
-//                     .verify(ndm_smt.root_hash())
-//                     .expect("Unable to verify proof")
-//             })
-//         });
-//     }
-
-//     for t in thread_counts {
-//         let ndm_smt = setup::build_ndm_smt(Height::from(16), MaxThreadCount::from(t), num_entities);
-//         let entity_id = EntityId::from_str("foo").unwrap();
-//         let proof = setup::generate_proof(&ndm_smt, &entity_id);
-
-//         group.bench_function(BenchmarkId::new("max_thread_count", t), |bench| {
-//             bench.iter(|| {
-//                 proof
-//                     .verify(ndm_smt.root_hash())
-//                     .expect("Unable to verify proof")
-//             })
-//         });
-//     }
-
-//     for u in NUM_USERS {
-//         let ndm_smt = setup::build_ndm_smt(Height::from(16), MaxThreadCount::default(), u);
-//         let entity_id = EntityId::from_str("foo").unwrap();
-//         let proof = setup::generate_proof(&ndm_smt, &entity_id);
-
-//         group.bench_function(BenchmarkId::new("num_users", u), |bench| {
-//             bench.iter(|| {
-//                 proof
-//                     .verify(ndm_smt.root_hash())
-//                     .expect("Unable to verify proof")
-//             })
-//         });
-//     }
-
-// group.finish();
-// }
-
-// TODO: add bench_verify_proof benches
+    println!(
+        "Memory usage: {} allocated / {} active / {} resident",
+        setup::bytes_as_string(alloc),
+        setup::bytes_as_string(act),
+        setup::bytes_as_string(res)
+    );
+}
 
 // ================================================================================================
 
-// fn bench_test_jemalloc_readings() {
-//     let e = epoch::mib().unwrap();
-//     let alloc = stats::allocated::mib().unwrap();
-//     let act = stats::active::mib().unwrap();
-//     let res = stats::resident::mib().unwrap();
+criterion_group!(benches, bench_dapol);
 
-//     // 1 MB
-//     let buf: Vec<u8> = Vec::with_capacity(1024u32.pow(2) as usize);
-
-//     e.advance().unwrap();
-
-//     println!(
-//         "buf capacity: {:<6}",
-//         setup::bytes_as_string(buf.capacity())
-//     );
-
-//     let alloc = alloc.read().unwrap();
-//     let act = act.read().unwrap();
-//     let res = res.read().unwrap();
-
-//     println!(
-//         "Memory usage: {} allocated / {} active / {} resident",
-//         setup::bytes_as_string(alloc),
-//         setup::bytes_as_string(act),
-//         setup::bytes_as_string(res)
-//     );
-// }
-
-// ================================================================================================
-
-criterion_group!(
-    benches,
-    bench_dapol,
-    // bench_generate_proof,
-    // bench_verify_proof
-);
-
-criterion_main!(benches /* bench_test_jemalloc_readings */,);
+criterion_main!(benches, bench_test_jemalloc_readings);
